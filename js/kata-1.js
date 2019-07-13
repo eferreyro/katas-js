26c16501c5572f356bd8a9fbc6bc24fac7693026
emafe7472@gmail.com <emafe7472@gmail.com>
2019-07-13 00:05:19 -0500

    Agregando las primeras katas

---
+++ js/kata-1.js
@@ -0,0 +1,4 @@
+// 12-07-2019
+ 
+// Congigna:
+// Algoritmo que lea tres notas y calcule e imprima el promedio de ellas.
---
+++ js/kata-3.js
@@ -0,0 +1,7 @@
+// 12-07-2019
+ 
+// Congigna:
+// Algoritmo que calcule lo que gana un empleado con base a las horas trabajadas teniendo en cuenta
+//  que cada hora se paga a 2000, adicionalmente se le realiza unos descuentos con respecto a un
+//  impuesto de seguridad del 10% sobre su salario. el sistema debe imprimir un mensaje indicando 
+//  el nombre del empleado y su sueldo total.
