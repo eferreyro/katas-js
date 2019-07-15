8b736bd04c89fdd9a061216f127f6aac30998f35
emmafe7472 <emafe7472@gmail.com>
2019-07-15 14:00:25 -0500

    Resolviendo Kata1

--- js/kata-1.js
+++ js/kata-1.js
@@ -1,4 +1,15 @@
 // 12-07-2019
  //Crear un codigo que reciba un texto y lo muestre de 3 formas distintas en el DOM
 
+//Caja de alerta pidiendo el dato de entrada
+ var texto = prompt('Ingrese un texto');
+
+//metodo 1 de mostrar el mensaje por CONSOLA
+ console.log(texto);
+ //Metodo 2 de mostrar el texto en un mensaje de ALERTA
+ alert(texto);
+//Metodo 3 de mostrar el mensaje en el DOM
+document.write(texto)
+
+
 
