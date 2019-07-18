b5bdd70c8228cd61af81ca836ee95a809e206eb0
emmafe7472 <emafe7472@gmail.com>
2019-07-18 17:59:22 -0500

    Agregando cosas interesantes al proyecto

---
+++ book/Code-Katas.js
@@ -0,0 +1,17 @@
+/*Principios del Code Kata
+Reserva 45 minutos sin interrupciones.
+Que la tareas a ejecutar sea muy simple.
+Que la puedas realizar varias veces y de diferentes maneras.
+Que tengas un feedback sobre lo que estás haciendo (aquí entra el testing).
+Que construyas en pasos cortos, ya que esto motiva a continuar.
+Que cuando hayas terminado sepas más de lo que sabías al empezar.
+¿A que es obvio y simple?. Pues sobre estos principios tenemos una gran herramienta para probar, equivocarnos, investigar y aprender. Pero Dave no solamente compartió sus reflexiones si no que fue publicando semanalmente un listado de ejercicios a los que llamó Code-Katas. Kata es el nombre que se le da a un tipo de ejercicios de Karate que consisten en repeticiones de ciertas posiciones y movimientos, buscando la mejora constante en pequeños pasos; y es el nombre que eligió Dave y con el que conocemos a esta técnica de aprendizaje.
+
+Ventajas y precauciones
+La principal ventaja de esta manera de prácticar es que nos acostumbra a atacar los problemas de múltiples formas, sin prisas, sin estar pendiente de los errores o de romper nuestro trabajo o el trabajo de algún compañero. Sin pensar en documentación, análisis o metodología. Disfrutar del reto intelectual, como si fuera un crucigrama o una buena lectura. También el tener una lista interminable de Katas publicadas en la Web, nos permite enfrentarnos a retos a los cuales no estamos habituados en nuestro trabajo diario o, simplemente, no se nos hubiera ocurrido.
+
+La llegada de TDD le ha dado una vuelta de tuerca a las Code-Katas ya que le sumamos el ciclo red-green-refactor y utilizamos la práctica para hacernos a esta nueva técnica de desarrollo que es tan extraña para quien no está acostumbrada a ella.
+
+La única precaución que se debe tener en cuenta es que estamos haciendo prácticas con ejemplos ideales y dirigidos. Es decir, muy raramente nos vamos a encontrar problemas tan simples y sencillos en el mundo real. Y, como dice el dicho, la realidad siempre supera la ficción. También hay que no caer en la tentación de igualar Code-Kata con TDD. Se puede hacer una Code-Kata sin tan siquiera test unitarios, siempre que el feedback sea el suficiente.
+
+Practicar, practicar, practicar.*/
---
+++ book/palabras-reservadas.js
@@ -0,0 +1,2 @@
+/*break, case, catch, continue, default, delete, do, else, finally, for, function, if, in, instanceof, 
+new, return, switch, this, throw, try, typeof, var, void, while, with*/
