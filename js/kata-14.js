417d3a2842add37f11787fe54fa2935e65be4029
emmafe7472 <emafe7472@gmail.com>
2019-07-19 12:25:29 -0500

    Resolviendo Kata 5

--- js/kata-14.js
+++ js/kata-14.js
@@ -2,7 +2,7 @@
 
 /*Hacer un algoritmo que lea un texto de entrada que tenga puntos entre palabras y los reemplace por un guion bajo */
 
-var texto=prompt('Por favor ingrese texto con puntos');
+var texto=prompt('Por favor, ingrese texto con puntos');
 var textoConGuion=texto.split(".");
 for(var i=0;i<textoConGuion.length;i++){
     document.write(`${textoConGuion[i]}_`);
