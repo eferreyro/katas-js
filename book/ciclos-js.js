02e96a0d461ed4085a23d8e6ab0822e8841f233d
eferreyro <emafe7472@gmail.com>
2019-07-27 18:22:32 -0500

    Agregando info basica al repo

--- book/ciclos-js.js
+++ book/ciclos-js.js
@@ -17,12 +17,22 @@ case
  Un bucle for se repite hasta que la condición especificada se evalúa como false 
  for ([expresionInicial]; [condicion]; [expresionIncremento])
   sentencia
- */
+ 
 
 let number= 1; //indico el valor inicial del ciclo
 
 for (i=number; i<=10; i++){
         document.write(`<br>${i}</br>`)
     }
+
+var dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+for (i = 0; i <= dias.length -1; i++){
+    document.write( `<br>${dias[i]}</br>` )
+}
   
-    
+*/
+
+ /* BUCLE WHILE
+
+ 
+ */
