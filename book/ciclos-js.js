fa82714af0b5d33be53c66c377a752380332036d
eferreyro <emafe7472@gmail.com>
2019-07-26 22:03:32 -0500

    Agregando algo de info

---
+++ book/ciclos-js.js
@@ -0,0 +1,28 @@
+/* Definicion de ciclos:
+Los ciclos e iteraciones son una manera sencilla de lograr repeticiones de eventos o funciones
+para conseguir diversas operaciones repetidamente.
+*/
+
+/*Tipos de ciclos
+for
+do while
+while
+label
+break
+continue
+case
+*/
+
+ /*BUCLE FOR
+ Un bucle for se repite hasta que la condición especificada se evalúa como false 
+ for ([expresionInicial]; [condicion]; [expresionIncremento])
+  sentencia
+ */
+
+let number= 1; //indico el valor inicial del ciclo
+
+for (i=number; i<=10; i++){
+        document.write(`<br>${i}</br>`)
+    }
+  
+    
